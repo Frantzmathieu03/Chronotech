92bb610b8fa241d0ece146d92982d019dc55aaf4
andrewyerm <yermakovichandrew@gmail.com>
2024-09-11 21:25:37 -0400

    restructure client end

---
+++ client/src/main.jsx
@@ -0,0 +1,39 @@
+import ReactDOM from 'react-dom/client'
+// Bringing in the required imports from 'react-router-dom' to set up application routing behavior
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './app';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import SignUp from './pages/Signup';
+import TodoList from './pages/todoList';
+
+// Define the accessible routes, and which components respond to which URL
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+      },
+      {
+        path: '/Login',
+        element: <Login />,
+      },
+      {
+        path: '/SignUp',
+        element: <SignUp />,
+      },
+      {
+        path: '/TodoList',
+        element: <TodoList />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
