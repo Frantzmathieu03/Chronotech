d9d7b6d5b76c0612c59baa44bdf2f98288d3e0fb
LunarWyrm <englerprogramming@gmail.com>
2024-09-17 19:27:28 -0400

    Header Styling and Themes
    - Styled header and organized files
    - Light theme
    - Dark theme
    - Classic Theme

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import Header from './components/Header';
+import Theme from './components/Theme';
 
 function App() {
   return (
+    <Theme>
       <Header />
       <main>
         <Outlet />
       </main>
+    </Theme>
   );
 }
 
